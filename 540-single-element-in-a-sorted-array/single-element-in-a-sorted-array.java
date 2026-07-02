63da4adcabe568d24eb991005987e1f02d4e0c89
Yug gupta <yugguptaedu@gmail.com>
2026-07-02 23:07:47 +0530

    Time: 0 ms (100.00%) | Memory: 52.7 MB (89.83%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            // Make mid even
+            if (mid % 2 == 1) {
+                mid--;
+            }
+
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            } else {
+                high = mid;
+            }
+        }
+
+        return nums[low];
+    }
+}
