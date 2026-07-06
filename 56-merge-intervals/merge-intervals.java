6b55d6df8655e7f33baaebc343eaf7638c6e1bb4
Yug gupta <yugguptaedu@gmail.com>
2026-07-06 23:45:20 +0530

    Time: 7 ms (99.11%) | Memory: 49.5 MB (6.05%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,19 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
-        List<int[]> result = new ArrayList<>();
+        int index = 0;
 
-        for (int[] interval : intervals) {
-            if (result.isEmpty() || result.get(result.size() - 1)[1] < interval[0]) {
-                result.add(interval);
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[index][1] >= intervals[i][0]) {
+                intervals[index][1] = Math.max(intervals[index][1], intervals[i][1]);
             } else {
-                result.get(result.size() - 1)[1] =
-                    Math.max(result.get(result.size() - 1)[1], interval[1]);
+                index++;
+                intervals[index] = intervals[i];
             }
         }
 
-        return result.toArray(new int[result.size()][]);
+        return Arrays.copyOf(intervals, index + 1);
     }
 }
