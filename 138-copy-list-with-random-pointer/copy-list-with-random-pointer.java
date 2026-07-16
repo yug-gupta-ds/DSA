fee114ae69c856a740b1008440dafd8b6302bc54
Yug gupta <yugguptaedu@gmail.com>
2026-07-16 23:04:14 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (52.19%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        // Step 1: Insert copied nodes
+        Node curr = head;
+        while (curr != null) {
+            Node copy = new Node(curr.val);
+            copy.next = curr.next;
+            curr.next = copy;
+            curr = copy.next;
+        }
+
+        // Step 2: Copy random pointers
+        curr = head;
+        while (curr != null) {
+            if (curr.random != null) {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+
+        // Step 3: Separate the two lists
+        Node dummy = new Node(0);
+        Node copyCurr = dummy;
+        curr = head;
+
+        while (curr != null) {
+            Node copy = curr.next;
+
+            curr.next = copy.next;
+            copyCurr.next = copy;
+            copyCurr = copy;
+
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+}
