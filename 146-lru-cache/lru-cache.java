7f1c9d49d52d41fd0d285eabb4e662992986a799
Yug gupta <yugguptaedu@gmail.com>
2026-07-31 23:49:34 +0530

    Time: 47 ms (47.32%) | Memory: 131.3 MB (60.73%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,87 @@
+class LRUCache {
+
+    class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private int capacity;
+    private HashMap<Integer, Node> map;
+
+    private Node head;
+    private Node tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    // Remove node
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    // Insert after head
+    private void insert(Node node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    public int get(int key) {
+
+        if (!map.containsKey(key))
+            return -1;
+
+        Node node = map.get(key);
+
+        remove(node);
+        insert(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+
+        if (map.containsKey(key)) {
+
+            Node node = map.get(key);
+
+            node.value = value;
+
+            remove(node);
+            insert(node);
+
+        } else {
+
+            if (map.size() == capacity) {
+
+                Node lru = tail.prev;
+
+                remove(lru);
+
+                map.remove(lru.key);
+            }
+
+            Node node = new Node(key, value);
+
+            insert(node);
+
+            map.put(key, node);
+        }
+    }
+}
