2d49fc0e9bdf1b08f5b7d6e3f950501be8c07da1
Yug gupta <yugguptaedu@gmail.com>
2026-09-21 03:03:14 +0530

    Time: 215 ms (22.82%) | Memory: 111.4 MB (69.65%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class MedianFinder {
+
+    PriorityQueue<Integer> small;
+    PriorityQueue<Integer> large;
+
+    public MedianFinder() {
+        small = new PriorityQueue<>(Collections.reverseOrder());
+        large = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        small.add(num);
+
+        large.add(small.poll());
+
+        if (large.size() > small.size()) {
+            small.add(large.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (small.size() > large.size())
+            return small.peek();
+
+        return (small.peek() + large.peek()) / 2.0;
+    }
+}
